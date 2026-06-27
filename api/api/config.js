0bc91cb0cd201abbef13443486f19a94be81a004
vnshamsika <vnshamsika@gmail.com>
2026-06-27 11:25:01 +0530

    Change anonKey to use SUPABASE_KEY

--- api/api/config.js
+++ api/api/config.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json({
-    anonKey: process.env.SUPABASE_ANON_KEY
+    anonKey: process.env.SUPABASE_KEY
   });
 }
